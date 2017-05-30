f6c6af3055b959f0d5d289239d372ba41b87c5af
Richard Hanna <richard.hanna@elao.com>
2017-05-30 17:47:11 +0200

    81 - Show courses list

--- src/views/Course/ListView.vue
+++ src/views/Course/ListView.vue
@@ -3,22 +3,12 @@
     <h1 class="mdc-typography--display1">Courses list</h1>
 
     <ul class="mdc-list mdc-list--two-line mdc-list--avatar-list two-line-avatar-text-icon-demo">
-      <li class="mdc-list-item">
+      <li class="mdc-list-item" v-for="course in this.$data.courses">
         <span class="mdc-list-item__text">
-          Vacation Itinerary
-          <span class="mdc-list-item__text__secondary">Jan 10, 2014</span>
+          {{ course.title }}
+          <span class="mdc-list-item__text__secondary">{{ course.teachers }}</span>
         </span>
         <i href="#" class="mdc-list-item__end-detail material-icons" title="More info">
-          keyboard_arrow_right
-        </i>
-      </li>
-
-      <li class="mdc-list-item">
-        <span class="mdc-list-item__text">
-          Kitchen Remodel
-          <span class="mdc-list-item__text__secondary">Jan 20, 2014</span>
-        </span>
-        <i class="mdc-list-item__end-detail material-icons" title="More info">
           keyboard_arrow_right
         </i>
       </li>
@@ -27,11 +17,12 @@
 </template>
 
 <script>
+import data from '@/data/data';
+
 export default {
-  name: 'hello',
   data() {
     return {
-      msg: 'Welcome to Your Vue.js PWA',
+      courses: data.courses,
     };
   },
 };
