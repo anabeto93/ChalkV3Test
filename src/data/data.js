0cce30cb388f7522718d9c99e0950c8f23dfb006
Richard Hanna <richard.hanna@elao.com>
2017-05-30 17:46:56 +0200

    81 - Fix courses data

---
+++ src/data/data.js
@@ -0,0 +1,276 @@
+export default {
+  courses: [
+    {
+      id: 1,
+      title: 'LNGS-401: Phonological Theory (First Semester 2016/2017)',
+      teachers: 'Dr. Fusheini Hudu',
+      image: '',
+      sessions: [
+        {
+          id: 1,
+          title: 'Introduction: phonological units and representations; distinctive features; natural classes, markedness.',
+          webview: 'file:///android_asset/data/cursus2/course1/s1.html',
+          locked: false,
+          available: true,
+
+        },
+        {
+          id: 2,
+          title: 'Phonological processes: assimilation and harmony.',
+          webview: 'file:///android_asset/data/cursus2/course1/s2.html',
+          locked: true,
+          available: true,
+
+        },
+        {
+          id: 3,
+          title: 'Analysing phonological processes in rule-based generative phonology.',
+          webview: 'file:///android_asset/data/cursus2/course1/s3.html',
+          locked: true,
+          available: true,
+
+        },
+        {
+          id: 4,
+          title: 'Interaction of rules.',
+          webview: 'file:///android_asset/data/cursus2/course1/s4.html',
+          locked: true,
+          available: true,
+
+        },
+        {
+          id: 5,
+          title: 'Tone: A challenge to rule-based phonology.',
+          webview: 'file:///android_asset/data/cursus2/course1/s5.html',
+          locked: true,
+          available: true,
+
+        },
+        {
+          id: 6,
+          title: 'Autosegmental Phonology: application to tone.',
+          webview: 'file:///android_asset/data/cursus2/course1/s6.html',
+          locked: true,
+          available: false,
+
+        },
+        {
+          id: 7,
+          title: 'Autosegmental phonology and tonal rules.',
+          webview: 'file:///android_asset/data/cursus2/course1/s7.html',
+          locked: true,
+          available: false,
+
+        },
+        {
+          id: 8,
+          title: 'Autosegmental phonology: application to segmental phenomena.',
+          webview: 'file:///android_asset/data/cursus2/course1/s8.html',
+          locked: true,
+          available: false,
+
+        },
+        {
+          id: 9,
+          title: 'Optimality Theory: From rules to contraints.',
+          webview: 'file:///android_asset/data/cursus2/course1/s9.html',
+          locked: true,
+          available: false,
+
+        },
+        {
+          id: 10,
+          title: 'Optimality Theory: Basic concepts.',
+          webview: 'file:///android_asset/data/cursus2/course1/s10.html',
+          locked: true,
+          available: false,
+
+        },
+        {
+          id: 11,
+          title: 'Optimality Theory: Types of structural changes.',
+          webview: 'file:///android_asset/data/cursus2/course1/s11.html',
+          locked: true,
+          available: false,
+
+        },
+        {
+          id: 12,
+          title: 'Optimality Theory: Syllable structure processes.',
+          webview: 'file:///android_asset/data/cursus2/course1/s12.html',
+          locked: true,
+          available: false,
+
+        },
+      ],
+    },
+    {
+      id: 2,
+      title: 'Course Syllabus and Practice Data',
+      teachers: 'Dr. Fusheini Hudu',
+      image: '',
+      sessions: [
+        {
+          id: 1,
+          title: 'Course Syllabus',
+          webview: 'file:///android_asset/data/cursus2/course2/s1.html',
+          locked: false,
+          available: true,
+
+        },
+        {
+          id: 2,
+          title: 'Practice Data - Shona and Turkish',
+          webview: 'file:///android_asset/data/cursus2/course2/s2.html',
+          locked: false,
+          available: true,
+
+        },
+        {
+          id: 3,
+          title: 'Practice Data - Dagbani and Akan',
+          webview: 'file:///android_asset/data/cursus2/course2/s3.html',
+          locked: false,
+          available: true,
+
+        },
+      ],
+    },
+    {
+      id: 3,
+      title: 'Partie 1',
+      teachers: 'Pr. Gbelé OUATTARA',
+      image: '',
+      sessions: [
+        {
+          id: 1,
+          title: 'Notions topographiques de base',
+          webview: 'file:///android_asset/data/c3-c1-s1.html',
+          locked: false,
+          available: true,
+
+        },
+        {
+          id: 2,
+          title: 'Généralités sur les cartes topograpgiques',
+          webview: 'file:///android_asset/data/c3-c1-s2.html',
+          locked: true,
+          available: true,
+
+        },
+        {
+          id: 3,
+          title: 'Elaboration d\'un profil topograpgique',
+          webview: 'file:///android_asset/data/c3-c1-s3.html',
+          locked: true,
+          available: true,
+
+        },
+
+      ],
+    },
+    {
+      id: 4,
+      title: 'Partie 2',
+      teachers: 'Pr. Gbelé OUATTARA',
+      image: '',
+      sessions: [
+        {
+          id: 1,
+          title: 'Etudes des cartes géologiques et réalisation de coupes géologiques',
+          webview: 'file:///android_asset/data/c3-c2-s1.html',
+          locked: false,
+          available: true,
+
+        },
+        {
+          id: 2,
+          title: 'Eléments des cartes géologiques',
+          webview: 'file:///android_asset/data/c3-c2-s2.html',
+          locked: true,
+          available: true,
+
+        },
+        {
+          id: 3,
+          title: 'Interprétation des cartes géologiques',
+          webview: 'file:///android_asset/data/c3-c2-s3.html',
+          locked: true,
+          available: true,
+
+        },
+        {
+          id: 4,
+          title: 'Les colonnes stratigraphiques',
+          webview: 'file:///android_asset/data/c3-c2-s4.html',
+          locked: true,
+          available: true,
+
+        },
+        {
+          id: 5,
+          title: 'Principes de construction des coupes géologiques',
+          webview: 'file:///android_asset/data/c3-c2-s5.html',
+          locked: true,
+          available: true,
+
+        },
+      ],
+    },
+    {
+      id: 5,
+      title: 'Thèmes',
+      teachers: 'Pr. Gbelé OUATTARA',
+      image: '',
+      sessions: [
+        {
+          id: 1,
+          title: 'Thème 1: Les Structures TABULAIRES (horizontales)',
+          webview: 'file:///android_asset/data/c3-c3-s1.html',
+          locked: false,
+          available: true,
+
+        },
+        {
+          id: 2,
+          title: 'Thème 2: les structures monoclinales',
+          webview: 'file:///android_asset/data/c3-c3-s2.html',
+          locked: false,
+          available: true,
+
+        },
+        {
+          id: 3,
+          title: 'Thème 3: Les Structures plissées',
+          webview: 'file:///android_asset/data/c3-c3-s3.html',
+          locked: false,
+          available: true,
+
+        },
+        {
+          id: 4,
+          title: 'Thème 4: Les Structures discordantes',
+          webview: 'file:///android_asset/data/c3-c3-s4.html',
+          locked: false,
+          available: true,
+
+        },
+        {
+          id: 5,
+          title: 'Thème 5: Les Structures faillées',
+          webview: 'file:///android_asset/data/c3-c3-s5.html',
+          locked: false,
+          available: true,
+
+        },
+        {
+          id: 6,
+          title: 'Thème 6: Les chevauchements',
+          webview: 'file:///android_asset/data/c3-c3-s6.html',
+          locked: false,
+          available: true,
+        },
+      ],
+    },
+  ],
+};
