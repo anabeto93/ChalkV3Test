77bd472e8e4d61b09b3ca11df13cae83abe77732
Paul Boiseau <paulboiseau@gmail.com>
2017-06-19 17:50:21 +0200

    Enable vuex eslint

--- front/.eslintrc.js
+++ front/.eslintrc.js
@@ -33,6 +33,7 @@ module.exports = {
     'import/no-extraneous-dependencies': ['error', {
       'optionalDependencies': ['test/unit/index.js']
     }],
+    'no-param-reassign': ["error", { 'props': false }], // enable vuex to mutate parameter
     // allow debugger during development
     'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0
   }
