661882aa399e20d4afb680da08acc169b5f880e1
LamGui <guillaume.webdev@gmail.com>
2017-06-19 17:09:24 +0200

    Move apollo to src/config

---
+++ front/src/config/apollo.js
@@ -0,0 +1,10 @@
+import Vue from 'vue';
+import VueApollo from 'vue-apollo';
+
+import apolloClient from './apolloClient';
+
+export default new VueApollo({
+  defaultClient: apolloClient,
+});
+
+Vue.use(VueApollo);
