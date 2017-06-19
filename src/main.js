a30ac2dcbd34d734ed717060326705143eaae1ee
LamGui <guillaume.webdev@gmail.com>
2017-06-19 15:11:09 +0200

    Create ApolloClient and ApolloProvider instance

--- src/main.js
+++ src/main.js
@@ -1,9 +1,25 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue';
+import { ApolloClient, createNetworkInterface } from 'apollo-client';
+import VueApollo from 'vue-apollo';
 
 import App from './App';
 import router from './router';
+
+const apolloClient = new ApolloClient({
+  networkInterface: createNetworkInterface({
+    uri: 'http://localhost:8080/graphql',
+    transportBatching: true,
+  }),
+  connectDevTools: true,
+});
+
+const apolloProvider = new VueApollo({
+  defaultClient: apolloClient,
+});
+
+Vue.use(VueApollo);
 
 Vue.config.productionTip = false;
 
@@ -11,6 +27,7 @@ Vue.config.productionTip = false;
 new Vue({
   el: '#app',
   router,
+  apolloProvider,
   template: '<App/>',
   components: { App },
 });
