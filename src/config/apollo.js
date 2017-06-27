1e5cf58e10f27ca30714044e2ade078aa89c1ac8
Richard Hanna <richard.hanna@elao.com>
2017-06-27 21:47:41 +0200

    Init React and React router

--- src/config/apollo.js
+++
@@ -1,9 +0,0 @@
-import Vue from 'vue';
-import VueApollo from 'vue-apollo';
-import apolloClient from './apolloClient';
-
-export default new VueApollo({
-  defaultClient: apolloClient,
-});
-
-Vue.use(VueApollo);
