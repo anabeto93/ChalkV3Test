5507a0e31ab65c27360da84f4bda03ec324eaef3
Richard Hanna <richard.hanna@elao.com>
2017-06-29 18:05:02 +0200

    Install graphql appolo client

--- src/index.js
+++ src/index.js
@@ -1,8 +1,18 @@
 import React from 'react';
+import { ApolloProvider } from 'react-apollo';
 import ReactDOM from 'react-dom';
+
 import App from './App';
+import GraphqlClient from './config/GraphqlClient'
 import registerServiceWorker from './registerServiceWorker';
+
 import './index.css';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <ApolloProvider client={GraphqlClient}>
+    <App />
+  </ApolloProvider>,
+  document.getElementById('root')
+);
+
 registerServiceWorker();
