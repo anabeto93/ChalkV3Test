9ae5640b3afae22f6ea62f65bede2f823334967f
Richard Hanna <richard.hanna@elao.com>
2017-07-17 17:36:14 +0200

    Add an action creator for graphql courses query

--- src/containers/CourseScreen.js
+++ src/containers/CourseScreen.js
@@ -1,17 +1,37 @@
-import React, { Component } from 'react';
+import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
+import React, { Component } from 'react';
+
+import { getCoursesInformations } from '../actions/actionCreators';
 
 export class CourseScreen extends Component {
+  handleLoad = event => {
+    event.preventDefault();
+    this.props.dispatch(getCoursesInformations());
+  };
+
   render() {
     console.log('rendering CourseScreen');
+    const { isFetching, items } = this.props;
 
     return (
       <div>
-        <h1>Course</h1>
+        <h1>Courses</h1>
+        <button onClick={this.handleLoad}>Load courses</button>
+        <p>
+          {isFetching ? 'Loading...' : ''}
+        </p>
+        <p>
+          {items.length}
+        </p>
         <Link to="/">Home</Link>
       </div>
     );
   }
 }
 
-export default CourseScreen;
+function mapStateToProps({ courses: { isFetching, items } }) {
+  return { isFetching, items };
+}
+
+export default connect(mapStateToProps)(CourseScreen);
--- src/reducers/courses.js
+++ src/reducers/courses.js
@@ -1,10 +1,36 @@
-import { GET_COURSES_INFORMATIONS } from '../actions/actionCreators';
+import {
+  FAIL_GET_COURSES_INFORMATIONS,
+  RECEIVE_COURSES_INFORMATIONS,
+  REQUEST_COURSES_INFORMATIONS
+} from '../actions/actionCreators';
 
-export default function courses(state = [], action) {
+export default function courses(
+  state = {
+    isFetching: false,
+    items: []
+  },
+  action
+) {
   switch (action.type) {
-    case GET_COURSES_INFORMATIONS: {
-      const { courses } = action.payload;
-      return courses;
+    case REQUEST_COURSES_INFORMATIONS: {
+      return Object.assign({}, state, {
+        isFetching: true
+      });
+    }
+
+    case RECEIVE_COURSES_INFORMATIONS: {
+      console.log('RECEIVE_COURSES_INFORMATIONS', action.payload);
+      return Object.assign({}, state, {
+        isFetching: false,
+        items: action.payload.courses
+      });
+    }
+
+    case FAIL_GET_COURSES_INFORMATIONS: {
+      return Object.assign({}, state, {
+        isFetching: false,
+        items: []
+      });
     }
 
     default:
