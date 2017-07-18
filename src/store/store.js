18cb951ea3b56056df6b336bb1a1af06297a4de7
Richard Hanna <richard.hanna@elao.com>
2017-07-18 11:11:24 +0200

    Persist and retrieve the store from localstorage

--- src/store/store.js
+++ src/store/store.js
@@ -1,4 +1,5 @@
 import { applyMiddleware, compose, createStore } from 'redux';
+import { persistStore, autoRehydrate } from 'redux-persist';
 import thunkMiddleware from 'redux-thunk';
 
 import appReducer from '../reducers';
@@ -9,11 +10,14 @@ const store = createStore(
   defaultState,
   compose(
     applyMiddleware(thunkMiddleware),
+    autoRehydrate(),
     typeof window.__REDUX_DEVTOOLS_EXTENSION__ !== 'undefined'
       ? window.__REDUX_DEVTOOLS_EXTENSION__()
       : f => f
   )
 );
+
+persistStore(store);
 
 if (module.hot) {
   module.hot.accept('../reducers', () => {
