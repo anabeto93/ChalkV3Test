e7633ac39821f70060e56877edc28b197c18d374
Nico <nicolas.dievart@elao.com>
2017-07-21 11:02:40 +0200

    Add file to session for the api

--- back/tests/Infrastructure/Normalizer/SessionNormalizerTest.php
+++ back/tests/Infrastructure/Normalizer/SessionNormalizerTest.php
@@ -11,6 +11,7 @@
 namespace Tests\Infrastructure\Normalizer;
 
 use App\Domain\Model\Session;
+use App\Infrastructure\Normalizer\Session\FileNormalizer;
 use App\Infrastructure\Normalizer\SessionNormalizer;
 use PHPUnit\Framework\TestCase;
 use Tests\Factory\CourseFactory;
@@ -23,9 +24,17 @@ class SessionNormalizerTest extends TestCase
         $createdAt = new \DateTime();
         $course = CourseFactory::create();
         $session = new Session('uuid', 'session title', 'this is the content', $course, null, $createdAt);
+        $file1 = $this->prophesize(Session\File::class);
+        $file2 = $this->prophesize(Session\File::class);
+        $session->setFiles([$file1->reveal(), $file2->reveal()]);
+
+        // Mock
+        $fileNormalizer = $this->prophesize(FileNormalizer::class);
+        $fileNormalizer->normalize($file1->reveal())->shouldBeCalled()->willReturn(['file1']);
+        $fileNormalizer->normalize($file2->reveal())->shouldBeCalled()->willReturn(['file2']);
 
         // Normalizer
-        $sessionNormalier = new SessionNormalizer();
+        $sessionNormalier = new SessionNormalizer($fileNormalizer->reveal());
         $result = $sessionNormalier->normalize($session);
 
         $expected = [
@@ -33,7 +42,11 @@ class SessionNormalizerTest extends TestCase
             'title' => 'session title',
             'content' => 'this is the content',
             'createdAt' => $createdAt,
-            'updatedAt' => $createdAt
+            'updatedAt' => $createdAt,
+            'files' => [
+                ['file1'],
+                ['file2'],
+            ],
         ];
 
         $this->assertEquals($expected, $result);
