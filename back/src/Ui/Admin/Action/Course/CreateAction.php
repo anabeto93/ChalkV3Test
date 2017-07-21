d2e016b5fa0903eda8dfb50e483b13d1d8ef7373
Nico <nicolas.dievart@elao.com>
2017-07-21 10:01:16 +0200

    Init base

---
+++ back/src/Ui/Admin/Action/Course/CreateAction.php
@@ -0,0 +1,15 @@
+<?php
+
+/*
+ * This file is part of the ChalkboardEducation Application project.
+ *
+ * Copyright (C) ChalkboardEducation
+ *
+ * @author Elao <contact@elao.com>
+ */
+
+namespace App\Ui\Admin\Action\Course;
+
+class CreateAction
+{
+}
---
+++ back/src/Ui/Admin/Action/Course/ListAction.php
@@ -0,0 +1,16 @@
+<?php
+
+/*
+ * This file is part of the ChalkboardEducation Application project.
+ *
+ * Copyright (C) ChalkboardEducation
+ *
+ * @author Elao <contact@elao.com>
+ */
+
+namespace App\Ui\Admin\Action\Course;
+
+class ListAction
+{
+
+}
