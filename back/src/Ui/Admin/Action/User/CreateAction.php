65520f4d43bdb84d38417f35241192161b49f60c
Nico <nicolas.dievart@elao.com>
2017-07-24 14:07:24 +0200

    124 - Add user create action

---
+++ back/src/Ui/Admin/Action/User/CreateAction.php
@@ -0,0 +1,100 @@
+<?php
+
+/*
+ * This file is part of the ChalkboardEducation Application project.
+ *
+ * Copyright (C) ChalkboardEducation
+ *
+ * @author Elao <contact@elao.com>
+ */
+
+namespace App\Ui\Admin\Action\User;
+
+use App\Application\Adapter\CommandBusInterface;
+use App\Application\Command\User\Create;
+use App\Domain\Exception\User\PhoneNumberAlreadyUsedException;
+use App\Ui\Admin\Form\Type\User\CreateType;
+use Symfony\Bundle\FrameworkBundle\Routing\Router;
+use Symfony\Bundle\FrameworkBundle\Templating\EngineInterface;
+use Symfony\Component\Form\FormError;
+use Symfony\Component\Form\FormFactoryInterface;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Session\Flash\FlashBag;
+use Symfony\Component\Translation\TranslatorInterface;
+
+class CreateAction
+{
+    /** @var EngineInterface */
+    private $engine;
+
+    /** @var CommandBusInterface */
+    private $commandBus;
+
+    /** @var FormFactoryInterface */
+    private $formFactory;
+
+    /** @var FlashBag */
+    private $flashBag;
+
+    /** @var Router */
+    private $router;
+
+    /** @var TranslatorInterface */
+    private $translator;
+
+    /**
+     * @param EngineInterface      $engine
+     * @param CommandBusInterface  $commandBus
+     * @param FormFactoryInterface $formFactory
+     * @param FlashBag             $flashBag
+     * @param Router               $router
+     * @param TranslatorInterface  $translator
+     */
+    public function __construct(
+        EngineInterface $engine,
+        CommandBusInterface $commandBus,
+        FormFactoryInterface $formFactory,
+        FlashBag $flashBag,
+        Router $router,
+        TranslatorInterface $translator
+    ) {
+        $this->engine = $engine;
+        $this->commandBus = $commandBus;
+        $this->formFactory = $formFactory;
+        $this->flashBag = $flashBag;
+        $this->router = $router;
+        $this->translator = $translator;
+    }
+
+    /**
+     * @param Request $request
+     *
+     * @return Response|RedirectResponse
+     */
+    public function __invoke(Request $request): Response
+    {
+        $create = new Create();
+        $form = $this->formFactory->create(CreateType::class, $create, [
+            'submit' => true,
+        ]);
+
+        if ($form->handleRequest($request)->isSubmitted() && $form->isValid()) {
+            try {
+                $this->commandBus->handle($create);
+                $this->flashBag->add('success', 'flash.admin.user.create.success');
+
+                return new RedirectResponse($this->router->generate('admin_user_list'));
+            } catch (PhoneNumberAlreadyUsedException $exception) {
+                $form->get('phoneNumber')->addError(new FormError(
+                    $this->translator->trans('validator.phoneNumber.alreadyUsed', [], 'validators')
+                ));
+            }
+        }
+
+        return $this->engine->renderResponse('Admin/User/create.html.twig', [
+            'form' => $form->createView()
+        ]);
+    }
+}
