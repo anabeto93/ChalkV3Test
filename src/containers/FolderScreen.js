309ab4b5c033a071506ecfe787c2a7700478a15e
Paul Boiseau <paulboiseau@gmail.com>
2017-07-25 16:28:39 +0200

    82 - Add course page title

--- src/containers/FolderScreen.js
+++ src/containers/FolderScreen.js
@@ -1,12 +1,22 @@
 import { Link } from "react-router-dom";
 import { List, ListItem } from "material-ui/List";
 import React, { Component } from "react";
-import { connect } from 'react-redux';
+import { connect } from "react-redux";
 import courseManager from "../services/CourseManager";
 
 export class FolderScreen extends Component {
+  constructor(props) {
+    super(props);
+
+    if (props.course !== undefined) {
+      document.title = props.course.title;
+    }
+  }
+
   render() {
     const course = this.props.course;
+
+    console.log('rendering FolderScreen');
 
     return (
       <div>
