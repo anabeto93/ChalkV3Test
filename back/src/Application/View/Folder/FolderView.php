5d0a0c9bd239163f5dc0bafcd8e2993259d3ec70
Nico <nicolas.dievart@elao.com>
2017-07-27 11:37:11 +0200

    130 - Add Folder list query handler

---
+++ back/src/Application/View/Folder/FolderView.php
@@ -0,0 +1,30 @@
+<?php
+
+/*
+ * This file is part of the ChalkboardEducation Application project.
+ *
+ * Copyright (C) ChalkboardEducation
+ *
+ * @author Elao <contact@elao.com>
+ */
+
+namespace App\Application\View\Folder;
+
+class FolderView
+{
+    /** @var int */
+    public $id;
+
+    /** @var string */
+    public $title;
+
+    /**
+     * @param int    $id
+     * @param string $title
+     */
+    public function __construct(int $id, string $title)
+    {
+        $this->id = $id;
+        $this->title = $title;
+    }
+}
