10582652224571c579253cc5e780e4859c2cf732
Paul Boiseau <paulboiseau@gmail.com>
2017-08-01 16:04:56 +0200

    Add routerResolver

--- src/containers/HomeScreen.js
+++ src/containers/HomeScreen.js
@@ -1,15 +1,10 @@
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
 import React, { Component } from 'react';
-import store from '../store/store';
-import { resetRoutingData } from '../actions/actionCreators';
 
 import LoginScreen from './LoginScreen';
 
 export class HomeScreen extends Component {
-  componentDidMount() {
-    store.dispatch(resetRoutingData());
-  }
 
   render() {
     console.log('rendering HomeScreen');
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -3,15 +3,13 @@ import { combineReducers } from 'redux';
 import courses from './courses';
 import currentUser from './currentUser';
 import updates from './updates';
-import routing from './routing'
 import GraphqlClient from '../graphql/client/GraphqlClient';
 
 const appReducer = combineReducers({
   apollo: GraphqlClient.reducer(),
   courses,
   currentUser,
-  updates,
-  routing
+  updates
 });
 
 export default appReducer;
