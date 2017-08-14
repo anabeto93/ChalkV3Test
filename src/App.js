cdc615047a97532fe20af0120389163751d810bb
Paul Boiseau <paulboiseau@gmail.com>
2017-08-14 15:21:36 +0200

    119 - Add route not found message

--- src/App.js
+++ src/App.js
@@ -1,21 +1,21 @@
-import React, { Component } from 'react';
 import getMuiTheme from 'material-ui/styles/getMuiTheme';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import React, { Component } from 'react';
 import { Provider } from 'react-redux';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import './App.css';
+import Header from './components/Header';
+import NotFound from './components/NotFound';
+import * as routes from './config/routes';
 
 import CourseScreen from './containers/CourseScreen';
-import HomeScreen from './containers/HomeScreen';
 import FolderScreen from './containers/FolderScreen';
-import SessionScreen from './containers/SessionScreen';
-import SessionDetailScreen from './containers/SessionDetailScreen';
+import HomeScreen from './containers/HomeScreen';
 import LoginScreen from './containers/LoginScreen';
 import PrivateRoute from './containers/PrivateRoute';
+import SessionDetailScreen from './containers/SessionDetailScreen';
+import SessionScreen from './containers/SessionScreen';
 import store from './store/store';
-import Header from './components/Header';
-import Updates from './components/Updates/Updates';
-import * as routes from './config/routes';
-import './App.css';
 
 const PRIMARY_COLOR = '#fc3691';
 
@@ -34,34 +34,37 @@ class App extends Component {
           <Router>
             <div>
               <Header />
-              {/*<Updates />*/}
-              <Route exact path={routes.HOME} component={HomeScreen} />
-              <Route exact path={routes.LOGIN} component={LoginScreen} />
-              <PrivateRoute
-                exact
-                path={routes.COURSES}
-                component={CourseScreen}
-              />
-              <PrivateRoute
-                exact
-                path={routes.FOLDER_LIST}
-                component={FolderScreen}
-              />
-              <PrivateRoute
-                exact
-                path={routes.SESSION_LIST}
-                component={SessionScreen}
-              />
-              <PrivateRoute
-                exact
-                path={routes.SESSION_LIST_WITHOUT_FOLDER}
-                component={SessionScreen}
-              />
-              <PrivateRoute
-                exact
-                path={routes.SESSION_DETAIL}
-                component={SessionDetailScreen}
-              />
+              <Switch>
+                {/*<Updates />*/}
+                <Route exact path={routes.HOME} component={HomeScreen} />
+                <Route exact path={routes.LOGIN} component={LoginScreen} />
+                <PrivateRoute
+                  exact
+                  path={routes.COURSES}
+                  component={CourseScreen}
+                />
+                <PrivateRoute
+                  exact
+                  path={routes.FOLDER_LIST}
+                  component={FolderScreen}
+                />
+                <PrivateRoute
+                  exact
+                  path={routes.SESSION_LIST}
+                  component={SessionScreen}
+                />
+                <PrivateRoute
+                  exact
+                  path={routes.SESSION_LIST_WITHOUT_FOLDER}
+                  component={SessionScreen}
+                />
+                <PrivateRoute
+                  exact
+                  path={routes.SESSION_DETAIL}
+                  component={SessionDetailScreen}
+                />
+                <Route component={NotFound} />
+              </Switch>
             </div>
           </Router>
         </MuiThemeProvider>
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,14 +1,14 @@
-import React, { Component } from 'react';
-import { withRouter } from 'react-router-dom';
-import { connect } from 'react-redux';
 import AppBar from 'material-ui/AppBar';
 import IconButton from 'material-ui/IconButton';
 import Back from 'material-ui/svg-icons/navigation/chevron-left';
 import UserIcon from 'material-ui/svg-icons/social/person';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { withRouter } from 'react-router-dom';
 
 import logoImage from '../assets/logo.png';
-import RouteResolver from '../services/RouteResolver';
 import { COURSES } from '../config/routes';
+import RouteResolver from '../services/RouteResolver';
 
 class Header extends Component {
   courseList() {
@@ -23,7 +23,12 @@ class Header extends Component {
         <img
           src={logoImage}
           alt="Chalkboard Education"
-          style={{ paddingTop: '10px', float: 'left', maxHeight: '50%', margin: '6px' }}
+          style={{
+            paddingTop: '10px',
+            float: 'left',
+            maxHeight: '50%',
+            margin: '6px'
+          }}
         />{' '}
         {title}
       </span>
@@ -69,8 +74,12 @@ class Header extends Component {
 }
 
 function mapStateToProps(state, props) {
+  let title = '';
   let route = RouteResolver.resolve(props.location);
-  let title = RouteResolver.resolveTitle(route);
+
+  if (route !== undefined) {
+    title = RouteResolver.resolveTitle(route);
+  }
 
   return { title };
 }
