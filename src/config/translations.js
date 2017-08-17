376e668a463922044517447fc759a98ff1ab82b7
Paul Boiseau <paulboiseau@gmail.com>
2017-08-17 17:36:13 +0200

    144 - Add settings locale to redux store

--- src/config/translations.js
+++ src/config/translations.js
@@ -3,7 +3,8 @@ import fr from './translate/fr.json';
 import en from './translate/en.json';
 
 export const availableLocales = { fr: 'Français', en: 'English' };
+export const defaultLocale = 'en';
 
 I18n.fallbacks = true;
-I18n.defaultLocale = 'en';
+I18n.defaultLocale = defaultLocale;
 I18n.translations = { fr, en };
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -3,13 +3,15 @@ import { combineReducers } from 'redux';
 import courses from './courses';
 import currentUser from './currentUser';
 import updates from './updates';
+import settings from './settings';
 import GraphqlClient from '../graphql/client/GraphqlClient';
 
 const appReducer = combineReducers({
   apollo: GraphqlClient.reducer(),
   courses,
   currentUser,
-  updates
+  updates,
+  settings
 });
 
 export default appReducer;
