06ca00ac789253bc5eecb98b08be80e4aca8e438
Richard Hanna <richard.hanna@elao.com>
2017-08-17 15:05:44 +0200

    137 - Create session text and files in the spool

--- src/reducers/courses.js
+++ src/reducers/courses.js
@@ -26,22 +26,48 @@ export default function courses(
     }
 
     case RECEIVE_COURSES_INFORMATIONS: {
-      const previousCourseItems = state.items;
+      // @todo: copy previous session content to new session content
+      // const previousCourseItems = state.items;
+
+      const lastUpdatedAt = state.updatedAt;
       const newCourseItems = action.payload.courses;
+      const sessionText = state.spool.sessionText;
+      const sessionFiles = state.spool.sessionFiles;
 
-      // copy previous session content to new session content
+      newCourseItems.forEach(course => {
+        course.folders.forEach(folder => {
+          folder.sessions.forEach(session => {
+            // Add session uuid to spool/sessionText
+            if (
+              isUpToDate(lastUpdatedAt, session.contentUpdatedAt) &&
+              !sessionText.includes(session.uuid)
+            ) {
+              sessionText.push(session.uuid);
+            }
 
-      // forEach Session // when contentUpdatedAt > lastUpdate -> add session uuid to spool/sessionText
-      // forEach SessionFile // when contentUpdatedAt > lastUpdate -> add file url to spool/sessionFiles
+            // Add file url to spool/sessionFiles
+            session.files.forEach(file => {
+              if (
+                isUpToDate(lastUpdatedAt, file.updatedAt) &&
+                !sessionFiles.includes(file.url)
+              ) {
+                sessionFiles.push(file.url);
+              }
+            });
+          });
+        });
+      });
 
       return {
         ...state,
+        // @todo: set new updateAt date provided by the backend
+        // updatedAt: newCourseItems.updatedAt
         isFetching: false,
         isErrorFetching: false,
         items: newCourseItems,
         spool: {
-          sessionText: ['hello'],
-          sessionFiles: ['hola', 'hello2']
+          sessionText: sessionText,
+          sessionFiles: sessionFiles
         }
       };
     }
@@ -57,4 +83,16 @@ export default function courses(
     default:
       return state;
   }
+}
+
+/**
+ * @param {string} lastUpdatedAt
+ * @param {string} contentUpdatedAt
+ * @returns {boolean}
+ */
+function isUpToDate(lastUpdatedAt, contentUpdatedAt) {
+  return (
+    null === lastUpdatedAt ||
+    new Date(contentUpdatedAt) < new Date(lastUpdatedAt)
+  );
 }
