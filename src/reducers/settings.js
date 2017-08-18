81d680ebc97a68b52e624178b591620d05f82f60
Paul Boiseau <paulboiseau@gmail.com>
2017-08-18 17:06:58 +0200

    144 - Use key in payload data

--- src/reducers/settings.js
+++ src/reducers/settings.js
@@ -3,7 +3,7 @@ import { SETTINGS_SET_LOCALE } from '../actions/actionCreators';
 export default function settings(state = { settings }, action) {
   switch (action.type) {
     case SETTINGS_SET_LOCALE:
-      return { ...state, locale: action.payload };
+      return { ...state, locale: action.payload.locale };
     default:
       return state;
   }
