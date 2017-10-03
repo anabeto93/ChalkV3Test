72c3c85ec162228d6c95147ced9983e210f80da7
Richard Hanna <richard.hanna@elao.com>
2017-10-03 22:58:30 +0200

    162 - Code generator encapsulating session uuid and user uuid

--- src/services/session/UnBlockSession.js
+++ src/services/session/UnBlockSession.js
@@ -1,10 +1,65 @@
 import getConfig from '../../config/index';
 
 export class UnBlockSession {
-  static getUnlockCodeForSession(userUuid, sessionUuid) {
-    const privateKey = getConfig().api.privateKey;
+  static getHexString() {
+    return '0123456789abcdef';
+  }
 
-    return sessionUuid;
+  static getUnlockCodeForSession(userUuid, sessionUuid) {
+    return this.getCodeFromUuid(userUuid) + this.getCodeFromUuid(sessionUuid);
+  }
+
+  static getCodeFromUuid(uuid) {
+    const privateKey = getConfig().privateKey;
+    const hexString = this.getHexString();
+
+    const remainingLength = privateKey.length - hexString.length;
+    const begin = Math.floor(Math.random() * remainingLength);
+    const firstChar = privateKey.charAt(begin);
+
+    let chars = uuid.split('');
+
+    // remove '-'
+    chars = chars.filter(char => char !== '-');
+
+    // replace a, b, c, d, e, f by 10, 11, 12, 13, 14, 15
+    chars = chars.map(char => hexString.indexOf(char));
+
+    return (
+      firstChar +
+      chars.map(char => privateKey.charAt(begin + char + 1)).join('')
+    );
+  }
+
+  static getUuidFromCode(code) {
+    const privateKey = getConfig().privateKey;
+    const hexString = this.getHexString();
+
+    let chars = code.split('');
+    const firstChar = chars[0];
+    chars.shift();
+
+    const begin = privateKey.indexOf(firstChar);
+
+    chars = chars.map(char => privateKey.indexOf(char) - begin - 1);
+
+    const result = chars.map(char => hexString.charAt(char)).join('');
+
+    return (
+      result.substr(0, 10) +
+      '-' +
+      result.substr(10, 5) +
+      '-' +
+      result.substr(15, 5)
+    );
+  }
+
+  static getSessionUuidFromCode(code) {
+    return this.getUuidFromCode(code.substr(code.length / 2, code.length));
+  }
+
+  static getUserUuidFromCode(code) {
+    return this.getUuidFromCode(code.substr(0, code.length / 2));
   }
 }
 
--- src/services/session/UnBlockSession.test.js
+++ src/services/session/UnBlockSession.test.js
@@ -3,7 +3,14 @@ import { expect } from 'chai';
 import UnBlockSession from './UnBlockSession';
 
 it('should return correct session uuid', () => {
-  expect(
-    UnBlockSession.getUnlockCodeForSession('userUuid', 'sessionUuid')
-  ).to.equal('sessionUuid');
+  const code = UnBlockSession.getUnlockCodeForSession(
+    'd1fae8c298-adb2e-e878b',
+    'a6342f0bf7-a2aae-5a485'
+  );
+
+  const userUuid = UnBlockSession.getUserUuidFromCode(code);
+  const sessionUuid = UnBlockSession.getSessionUuidFromCode(code);
+
+  expect(userUuid).to.equal('d1fae8c298-adb2e-e878b');
+  expect(sessionUuid).to.equal('a6342f0bf7-a2aae-5a485');
 });
