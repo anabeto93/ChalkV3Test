538fb2d8fad315d63697f98d0a2a7021ff97ace4
Paul Boiseau <paulboiseau@gmail.com>
2017-10-06 16:54:14 +0200

    164 - Show error when fail validating

--- front/src/containers/CourseScreen.js
+++ front/src/containers/CourseScreen.js
@@ -1,24 +1,34 @@
-import { connect } from 'react-redux';
+import I18n from 'i18n-js';
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
 import CoursesList from '../components/Course/CoursesList';
 import UserPanel from '../components/Course/UserPanel';
+import Error from '../components/Error';
 
 export class CourseScreen extends Component {
   render() {
-    const { courses } = this.props;
+    const { courses, isFailValidating, locale } = this.props;
 
     return (
       <div>
-        <UserPanel/>
-        <CoursesList courses={courses}/>
+        <UserPanel />
+        <CoursesList courses={courses} />
+        {isFailValidating &&
+          <Error
+            show={true}
+            message={I18n.t('send.sms.validation.fail', { locale })}
+          />}
       </div>
     );
   }
 }
 
-function mapStateToProps({ content: { courses } }) {
-  return { courses };
+function mapStateToProps({
+  content: { courses, isFailValidating },
+  settings: { locale }
+}) {
+  return { courses, isFailValidating, locale };
 }
 
 export default connect(mapStateToProps)(CourseScreen);
