e8836c231b05418acb0ed9e1ec80daf46e63ad06
Paul Boiseau <paulboiseau@gmail.com>
2017-10-09 11:26:06 +0200

    164 - Fix unit test

--- src/services/session/UnBlockSession.test.js
+++ src/services/session/UnBlockSession.test.js
@@ -8,7 +8,8 @@ it('should return correct Session Uuid and User Uuid', () => {
 
   const code = UnBlockSession.getUnlockCodeForSession(
     'd1fae8c298-adb2e-e878b',
-    'a6342f0bf7-a2aae-5a485'
+    'a6342f0bf7-a2aae-5a485',
+    appPrivateKey
   );
 
   const userUuid = UnBlockSession.getUserUuidFromCode(code, appPrivateKey);
