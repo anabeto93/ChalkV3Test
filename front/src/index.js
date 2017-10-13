aaa4f9ecda03b187b65cd966d12e9b9d0a48e429
Paul Boiseau <paulboiseau@gmail.com>
2017-10-13 16:46:03 +0200

    Catch javascript exception only on production build

--- front/src/index.js
+++ front/src/index.js
@@ -1,17 +1,19 @@
 import 'babel-polyfill';
+import Raven from 'raven-js';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import injectTapEventPlugin from 'react-tap-event-plugin';
-import Raven from 'raven-js';
 
 import App from './App';
+import getConfig, { isProduction } from './config/index';
 import './config/translations';
 import './index.css';
-import getConfig from './config/index';
 
 import registerServiceWorker from './registerServiceWorker';
 
-Raven.config(getConfig().externalServices.sentry.dsn).install();
+if (isProduction) {
+  Raven.config(getConfig().externalServices.sentry.dsn).install();
+}
 
 injectTapEventPlugin();
 
