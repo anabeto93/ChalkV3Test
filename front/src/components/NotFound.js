d6ea0a31dadbd2f6db8114239d60c179295ce07b
Richard Hanna <richard.hanna@elao.com>
2017-10-26 10:22:36 +0200

    Fix 404 screen UI

--- front/src/components/NotFound.js
+++ front/src/components/NotFound.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const NotFound = function() {
   return (
-    <div>
+    <div className="screen-centered">
       <h3>404 page not found</h3>
       <p>We are sorry but the page you are looking for does not exist.</p>
     </div>
