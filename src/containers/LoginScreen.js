ce7ece10e1ad54183daab8146787cbae91623e35
Richard Hanna <richard.hanna@elao.com>
2017-10-26 10:22:21 +0200

    Rework login screen

--- src/containers/LoginScreen.js
+++ src/containers/LoginScreen.js
@@ -2,15 +2,11 @@ import I18n from 'i18n-js';
 import { RaisedButton } from 'material-ui';
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import {
-  getUserInformations,
-  purgeUserInformations
-} from '../actions/actionCreators';
+import { getUserInformations, getUpdates } from '../actions/actionCreators';
 import UserPanel from '../components/Course/UserPanel';
 
 import { COURSES, HOME } from '../config/routes';
 import { LOGIN_STATE_LOGGED_IN } from '../store/defaultState';
-import { Redirect } from 'react-router-dom';
 
 class LoginScreen extends Component {
   constructor(...args) {
@@ -20,18 +16,31 @@ class LoginScreen extends Component {
 
   componentDidMount() {
     const { currentUser } = this.props;
+    const token = this.props.match.params.token;
 
-    if (currentUser.loginState !== LOGIN_STATE_LOGGED_IN) {
-      this.setState({ isFetching: true });
-      this.props.dispatch(getUserInformations(this.props.match.params.token));
+    if (null === token) {
+      this.props.history.push(HOME);
     }
+
+    const isTokenChanged =
+      currentUser.token !== null && currentUser.token !== token;
+
+    if (isTokenChanged || currentUser.loginState !== LOGIN_STATE_LOGGED_IN) {
+      this.setState({ isFetching: true });
+      this.props.dispatch(getUserInformations(token));
+
+      return;
+    }
+
+    // user already logged, redirect to courses list
+    this.props.history.push(COURSES);
   }
 
   componentWillReceiveProps(nextProps) {
-    const { token: previousToken } = this.props.currentUser;
     const { token: nextToken, loginState } = nextProps.currentUser;
 
-    if (null !== previousToken && nextToken === null) {
+    if (nextToken === null) {
+      console.log('push home');
       this.props.history.push(HOME);
     }
 
@@ -41,19 +50,15 @@ class LoginScreen extends Component {
   }
 
   handleRedirectCourses = () => {
+    // Get updates
+    this.props.dispatch(getUpdates(null));
+
+    // Go to courses list
     this.props.history.push(COURSES);
   };
 
   render() {
     const { locale, currentUser } = this.props;
-    const { token } = this.props.match.params;
-
-    if (currentUser.token === token) {
-      return <Redirect to={HOME} />;
-    } else if (currentUser.token !== null && currentUser.token !== token) {
-      this.props.dispatch(purgeUserInformations());
-      this.props.dispatch(getUserInformations(token));
-    }
 
     if (this.state.isFetching) {
       return (
@@ -65,7 +70,7 @@ class LoginScreen extends Component {
 
     if (currentUser.loginState === LOGIN_STATE_LOGGED_IN) {
       return (
-        <div>
+        <div className="screen-centered">
           <UserPanel />
           <RaisedButton
             label={I18n.t('login.start', { locale })}
