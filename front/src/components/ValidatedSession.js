289c03a1bae2edc9206cba01c0cd064778f74448
Richard Hanna <richard.hanna@elao.com>
2017-10-26 17:18:40 +0200

    103 - Create a ValidatedSession component

---
+++ front/src/components/ValidatedSession.js
@@ -0,0 +1,60 @@
+import I18n from 'i18n-js';
+import { RaisedButton } from 'material-ui';
+import Arrow from 'material-ui/svg-icons/hardware/keyboard-arrow-right';
+import { withRouter } from 'react-router-dom';
+import React, { Component } from 'react';
+
+import { SESSION_DETAIL, SESSION_LIST } from '../config/routes';
+import generateUrl from '../services/generateUrl';
+
+class ValidatedSession extends Component {
+  handleRedirectNextSession = () => {
+    if (null === this.props.nextSession) {
+      return;
+    }
+
+    return this.props.history.push(
+      generateUrl(SESSION_DETAIL, {
+        ':courseUuid': this.props.nextSession.courseUuid,
+        ':sessionUuid': this.props.nextSession.uuid
+      })
+    );
+  };
+
+  handleRedirectSessionList = () => {
+    this.props.history.push(
+      generateUrl(SESSION_LIST, {
+        ':courseUuid': this.props.session.courseUuid,
+        ':folderUuid': this.props.session.folderUuid
+      })
+    );
+  };
+
+  render() {
+    const { locale } = this.props;
+
+    return (
+      <div className="screen-centered">
+        <h4>
+          {I18n.t('send.validation.success', { locale })}
+        </h4>
+        <RaisedButton
+          style={{ float: 'left' }}
+          label={I18n.t('send.sessionListButton', { locale })}
+          onClick={this.handleRedirectSessionList}
+        />
+        {null !== this.props.nextSession &&
+          <RaisedButton
+            className="button-primary"
+            primary={true}
+            onClick={this.handleRedirectNextSession}
+            label={I18n.t('send.nextButton', { locale })}
+            labelPosition="before"
+            icon={<Arrow />}
+          />}
+      </div>
+    );
+  }
+}
+
+export default withRouter(ValidatedSession);
