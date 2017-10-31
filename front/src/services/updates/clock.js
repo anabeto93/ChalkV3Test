7a2f9e469f54cc081645ecaa8c67ef1127ce2026
Richard Hanna <richard.hanna@elao.com>
2017-10-31 14:48:02 +0100

    Add more delay before check updates

--- front/src/services/updates/clock.js
+++ front/src/services/updates/clock.js
@@ -4,8 +4,8 @@ export default function() {
   // Loop every 5 minutes
   const loopCheckUpdates = setInterval(checkUpdates, 300000);
 
-  // Check updates when app started, after 3s waiting the store is rehydrated by Redux Persist
-  const firstCheckUpdates = setTimeout(checkUpdates, 1000);
+  // Check updates when app start
+  const firstCheckUpdates = setTimeout(checkUpdates, 5000);
 
   if (module.hot) {
     module.hot.accept();
