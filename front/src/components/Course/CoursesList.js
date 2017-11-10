4592d93d62620831888d83e8fd1fb2b14fd352fa
Richard Hanna <richard.hanna@elao.com>
2017-11-10 16:09:50 +0100

    Do not show courses list when no course

--- front/src/components/Course/CoursesList.js
+++ front/src/components/Course/CoursesList.js
@@ -1,34 +1,43 @@
+import I18n from 'i18n-js';
 import { List, ListItem } from 'material-ui/List';
 import Arrow from 'material-ui/svg-icons/hardware/keyboard-arrow-right';
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+
 import generateUrl from '../../services/generateUrl';
 import { FOLDER_LIST } from '../../config/routes';
 
 export class CoursesList extends Component {
   render() {
-    const { courses } = this.props;
+    const { courses, locale } = this.props;
+
+    if (undefined === courses || 0 === Object.keys(courses).length) {
+      return (
+        <p className="screen-centered alert">
+          {I18n.t('course.noContentAvailable', { locale })}
+        </p>
+      );
+    }
 
     return (
       <List>
-        {undefined !== courses &&
-          Object.keys(courses).map(key => {
-            let course = courses[key];
+        {Object.keys(courses).map(key => {
+          let course = courses[key];
 
-            return (
-              <Link
-                className="link-primary"
-                key={course.uuid}
-                to={generateUrl(FOLDER_LIST, { ':courseUuid': course.uuid })}
-              >
-                <ListItem
-                  primaryText={course.title}
-                  secondaryText={course.teacherName}
-                  rightIcon={<Arrow />}
-                />
-              </Link>
-            );
-          })}
+          return (
+            <Link
+              className="link-primary"
+              key={course.uuid}
+              to={generateUrl(FOLDER_LIST, { ':courseUuid': course.uuid })}
+            >
+              <ListItem
+                primaryText={course.title}
+                secondaryText={course.teacherName}
+                rightIcon={<Arrow />}
+              />
+            </Link>
+          );
+        })}
       </List>
     );
   }
