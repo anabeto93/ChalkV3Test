4750ce85ec64c6cb35ae0e8c51f35a28bce614f1
niiapa <abbeyniiapa@gmail.com>
2017-11-15 16:32:44 +0000

    Proceeds to quiz from session detail

--- src/components/SessionFooter.js
+++ src/components/SessionFooter.js
@@ -4,7 +4,12 @@ import Arrow from 'material-ui/svg-icons/hardware/keyboard-arrow-right';
 import React from 'react';
 import { connect } from 'react-redux';
 import { withRouter } from 'react-router-dom';
-import { SESSION_DETAIL, SESSION_LIST, SESSION_SEND } from '../config/routes';
+import {
+  SESSION_DETAIL,
+  SESSION_LIST,
+  SESSION_SEND,
+  QUESTION_DETAIL
+} from '../config/routes';
 import CourseManager from '../services/CourseManager';
 import generateUrl from '../services/generateUrl';
 
@@ -12,6 +17,16 @@ const footer = props => {
   const { courseUuid, session, sessions, history, locale } = props;
 
   const handleNext = () => {
+    if (session.questions) {
+      return history.push(
+        generateUrl(QUESTION_DETAIL, {
+          ':courseUuid': courseUuid,
+          ':sessionUuid': session.uuid,
+          ':questionIndex': 0
+        })
+      );
+    }
+
     if (session.needValidation && !session.validated) {
       return history.push(
         generateUrl(SESSION_SEND, {
@@ -43,7 +58,7 @@ const footer = props => {
   return (
     <footer className="next-session-footer background-grey">
       <RaisedButton
-        label={I18n.t('session.nextButton', { locale })}
+        label={I18n.t('session.quizButton', { locale })}
         labelPosition="before"
         primary={true}
         onClick={handleNext}
