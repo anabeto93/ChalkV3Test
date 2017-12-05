0b120e6140ea86a864ab1c269ede8876c2753811
niiapa <abbeyniiapa@gmail.com>
2017-12-05 13:38:50 +0000

    View checked answers

--- src/components/Quiz/QuestionAnswers.js
+++ src/components/Quiz/QuestionAnswers.js
@@ -7,18 +7,21 @@ const QuestionAnswers = props => {
     marginBottom: 16
   };
 
+  const userAnswers = question.userAnswers ? question.userAnswers : [];
+
   if (question.isMultiple) {
     return (
       <div>
         {Object.keys(question.answers).map((key, index) => {
-          const answer = question.answers[key];
+          const answer = question.answers[index];
 
           return (
             <Checkbox
               key={index}
               label={answer.title}
+              checked={userAnswers.includes(index)}
               onCheck={() => {
-                props.handleAnswerChange(key);
+                props.handleAnswerChange(index);
               }}
               style={optionStyle}
             />
@@ -31,18 +34,19 @@ const QuestionAnswers = props => {
   return (
     <RadioButtonGroup
       name={question.title}
+      valueSelected={userAnswers[0]}
       onChange={(e, value) => {
         props.handleAnswerChange(value);
       }}
     >
       {Object.keys(question.answers).map((key, index) => {
-        const answer = question.answers[key];
+        const answer = question.answers[index];
 
         return (
           <RadioButton
             key={index}
             label={answer.title}
-            value={key}
+            value={index}
             style={optionStyle}
           />
         );
