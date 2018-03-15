7ad9f9c42102382b4965c1287be897213950a708
niiapa <abbeyniiapa@gmail.com>
2018-03-15 15:14:17 +0000

    Changed institution view to list number of cohorts

--- back/src/Application/View/Institution/InstitutionView.php
+++ back/src/Application/View/Institution/InstitutionView.php
@@ -19,15 +19,20 @@ class InstitutionView {
     /** @var int */
     public $numberOfStudents;
 
+    /** @var int */
+    public $numberOfCohorts;
+
     /**
      * InstitutionView constructor.
      * @param int $id
      * @param string $name
      * @param int $numberOfStudents
+     * @param int $numberOfCohorts
      */
-    public function __construct(int $id, string $name, int $numberOfStudents) {
+    public function __construct(int $id, string $name, int $numberOfStudents, int $numberOfCohorts) {
         $this->id = $id;
         $this->name = $name;
         $this->numberOfStudents = $numberOfStudents;
+        $this->numberOfCohorts = $numberOfCohorts;
     }
 }
