389246b365a93153d536109b578c2fdcbf08ed38
niiapa <abbeyniiapa@gmail.com>
2018-03-16 18:30:26 +0000

    Fixed CohortListQueryHandler to include number of students

--- back/src/Application/Query/Cohort/CohortListQueryHandler.php
+++ back/src/Application/Query/Cohort/CohortListQueryHandler.php
@@ -11,25 +11,39 @@ namespace App\Application\Query\Cohort;
 
 use App\Application\View\Cohort\CohortView;
 use App\Domain\Repository\CohortRepositoryInterface;
+use App\Domain\Repository\CohortUserRepositoryInterface;
 
 class CohortListQueryHandler {
     /** @var CohortRepositoryInterface */
     public $cohortRepository;
 
+    /** @var CohortUserRepositoryInterface */
+    public $cohortUserRepository;
+
     /**
      * CohortListQueryHandler constructor.
      * @param CohortRepositoryInterface $cohortRepository
+     * @param CohortUserRepositoryInterface $cohortUserRepository
      */
-    public function __construct(CohortRepositoryInterface $cohortRepository) {
+    public function __construct(CohortRepositoryInterface $cohortRepository, CohortUserRepositoryInterface $cohortUserRepository) {
         $this->cohortRepository = $cohortRepository;
+        $this->cohortUserRepository = $cohortUserRepository;
     }
 
+    /**
+     * @param CohortListQuery $query
+     * @return array
+     */
     public function handle(CohortListQuery $query): array {
         $cohortViews = [];
         $cohorts = $this->cohortRepository->findByInstitution($query->institution);
 
         foreach($cohorts as $cohort) {
-            $cohortViews[] = new CohortView($cohort->getId(), $cohort->getTitle());
+            $cohortViews[] = new CohortView(
+                $cohort->getId(),
+                $cohort->getTitle(),
+                $this->cohortUserRepository->counterUserForCohort($cohort)
+            );
         }
 
         return $cohortViews;
