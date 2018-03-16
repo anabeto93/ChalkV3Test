2bf941e9d220c041ce970acfa73f5db0b610082f
niiapa <abbeyniiapa@gmail.com>
2018-03-16 14:47:57 +0000

    Make title unique in cohort table

--- back/src/Infrastructure/Migrations/Version20180316144604.php
+++ back/src/Infrastructure/Migrations/Version20180316144604.php
@@ -8,7 +8,7 @@ use Doctrine\DBAL\Schema\Schema;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-class Version20180315143013 extends AbstractMigration
+class Version20180316144604 extends AbstractMigration
 {
     /**
      * @param Schema $schema
@@ -18,7 +18,7 @@ class Version20180315143013 extends AbstractMigration
         // this up() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
-        $this->addSql('CREATE TABLE cohort (id INT AUTO_INCREMENT NOT NULL, institution_id INT NOT NULL, uuid VARCHAR(255) NOT NULL, title VARCHAR(255) NOT NULL, created_at DATETIME NOT NULL, updated_at DATETIME NOT NULL, UNIQUE INDEX UNIQ_D3B8C16BD17F50A6 (uuid), INDEX IDX_D3B8C16B10405986 (institution_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE cohort (id INT AUTO_INCREMENT NOT NULL, institution_id INT NOT NULL, uuid VARCHAR(255) NOT NULL, title VARCHAR(255) NOT NULL, created_at DATETIME NOT NULL, updated_at DATETIME NOT NULL, UNIQUE INDEX UNIQ_D3B8C16BD17F50A6 (uuid), UNIQUE INDEX UNIQ_D3B8C16B2B36786B (title), INDEX IDX_D3B8C16B10405986 (institution_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE = InnoDB');
         $this->addSql('ALTER TABLE cohort ADD CONSTRAINT FK_D3B8C16B10405986 FOREIGN KEY (institution_id) REFERENCES institution (id) ON DELETE CASCADE');
     }
 
