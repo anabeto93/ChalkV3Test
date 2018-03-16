27ad51f2261ae9ab48fd8bf1433d8da5e0201414
niiapa <abbeyniiapa@gmail.com>
2018-03-16 16:17:15 +0000

    CohortUser repository classes created and registered

---
+++ back/src/Domain/Repository/CohortUserRepositoryInterface.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: niiapa
+ * Date: 16/03/2018
+ * Time: 16:02
+ */
+
+namespace App\Domain\Repository;
+
+
+use App\Domain\Model\Cohort;
+use App\Domain\Model\CohortUser;
+
+interface CohortUserRepositoryInterface {
+    /**
+     * @param Cohort $cohort
+     * @return int
+     */
+    public function counterUserForCohort(Cohort $cohort): int;
+
+    /**
+     * @param Cohort $cohort
+     * @return array
+     */
+    public function findByCohort(Cohort $cohort): array;
+
+    /**
+     * @param CohortUser $cohortUser
+     * @return mixed
+     */
+    public function remove(CohortUser $cohortUser);
+}
