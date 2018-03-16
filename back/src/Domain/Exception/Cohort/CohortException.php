19d10152c43eec773be91aa52e245ccf54240654
niiapa <abbeyniiapa@gmail.com>
2018-03-16 14:54:32 +0000

    Preparing exceptions for cohorts with same names in Institutions

---
+++ back/src/Domain/Exception/Cohort/CohortException.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: niiapa
+ * Date: 16/03/2018
+ * Time: 14:50
+ */
+
+namespace App\Domain\Exception\Cohort;
+
+
+class CohortException extends \DomainException {
+
+}
