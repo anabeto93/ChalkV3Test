87bcd6d60e0bd4df170c16b71512a244d85f3850
niiapa <abbeyniiapa@gmail.com>
2018-03-18 20:25:05 +0000

    Created CourseChoiceType and AssignCourseType forms

--- back/src/Ui/Admin/Form/Type/Cohort/AssignCourseType.php
+++ back/src/Ui/Admin/Form/Type/Cohort/AssignCourseType.php
@@ -2,23 +2,23 @@
 /**
  * Created by PhpStorm.
  * User: niiapa
- * Date: 16/03/2018
- * Time: 17:59
+ * Date: 18/03/2018
+ * Time: 20:12
  */
 
 namespace App\Ui\Admin\Form\Type\Cohort;
 
 
-use App\Ui\Admin\Form\Type\User\UserChoiceType;
+use App\Ui\Admin\Form\Type\Course\CourseChoiceType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\FormBuilderInterface;
 
-class AssignUserType extends AbstractCohortType {
+class AssignCourseType extends AbstractCohortType {
     /**
      * @inheritdoc
      */
     public function buildForm(FormBuilderInterface $builder, array $options) {
-        $builder->add('users', UserChoiceType::class,  [
+        $builder->add('courses', CourseChoiceType::class, [
             'expanded' => true,
             'multiple' => true
         ])->add('submit', SubmitType::class);
---
+++ back/src/Ui/Admin/Form/Type/Course/CourseChoiceType.php
@@ -0,0 +1,58 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: niiapa
+ * Date: 18/03/2018
+ * Time: 20:15
+ */
+
+namespace App\Ui\Admin\Form\Type\Course;
+
+
+use App\Domain\Model\Course;
+use App\Domain\Repository\CourseRepositoryInterface;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\OptionsResolver\Options;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class CourseChoiceType extends AbstractType {
+    /** @var CourseRepositoryInterface */
+    private $courseRepository;
+
+    /**
+     * CourseChoiceType constructor.
+     * @param CourseRepositoryInterface $courseRepository
+     */
+    public function __construct(CourseRepositoryInterface $courseRepository) {
+        $this->courseRepository = $courseRepository;
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function configureOptions(OptionsResolver $resolver) {
+        $resolver->setDefaults([
+            'class' => Course::class,
+            'choice_label' => function(Course $course) {
+                return sprintf('%s (%s)',
+                    $course->getTitle(),
+                    $course->getTeacherName()
+                );
+            },
+            'repositoryMethod' => function(CourseRepositoryInterface $courseRepository) {
+                return $courseRepository->getAll();
+            },
+            'choices' => function(Options $options) {
+                return $options['repositoryMethod']($this->courseRepository);
+            }
+        ]);
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function getParent() {
+        return ChoiceType::class;
+    }
+}
