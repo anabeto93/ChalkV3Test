631ec99c3a657ab31b5f67b793234d05fbb042a0
niiapa <abbeyniiapa@gmail.com>
2018-04-26 16:13:34 +0000

    back - Add args for User query and handling tokenIssuedAt param in UserResolver

--- back/src/Infrastructure/GraphQL/Resolver/UserResolver.php
+++ back/src/Infrastructure/GraphQL/Resolver/UserResolver.php
@@ -10,9 +10,12 @@
 
 namespace App\Infrastructure\GraphQL\Resolver;
 
+use App\Application\Command\User\TokenIssuedAt;
+use App\Application\Command\User\TokenIssuedAtHandler;
 use App\Infrastructure\Normalizer\UserNormalizer;
 use App\Infrastructure\Security\Api\ApiUserAdapter;
 use GraphQL\Error\UserError;
+use Overblog\GraphQLBundle\Definition\Argument;
 use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorageInterface;
 
 class UserResolver
@@ -23,27 +26,40 @@ class UserResolver
     /** @var UserNormalizer */
     private $userNormalizer;
 
+    /** @var TokenIssuedAtHandler */
+    private $tokenIssuedAtHandler;
+
     /**
+     * UserResolver constructor.
      * @param TokenStorageInterface $tokenStorage
-     * @param UserNormalizer        $userNormalizer
+     * @param UserNormalizer $userNormalizer
+     * @param TokenIssuedAtHandler $tokenIssuedAtHandler
      */
-    public function __construct(TokenStorageInterface $tokenStorage, UserNormalizer $userNormalizer)
-    {
+    public function __construct(TokenStorageInterface $tokenStorage, UserNormalizer $userNormalizer, TokenIssuedAtHandler $tokenIssuedAtHandler) {
         $this->tokenStorage = $tokenStorage;
         $this->userNormalizer = $userNormalizer;
+        $this->tokenIssuedAtHandler = $tokenIssuedAtHandler;
     }
 
     /**
+     * @param Argument $args
+     *
      * @return array
      *
      * @throw UserError
      */
-    public function resolveUser(): array
+    public function resolveUser(Argument $args): array
     {
         $user = $this->tokenStorage->getToken()->getUser();
 
         if (!$user instanceof ApiUserAdapter) {
             throw new UserError('Invalid parameter');
+        }
+
+        if($args["tokenIssuedAt"]) {
+            $tokenIssuedAt = new TokenIssuedAt($user->getUser());
+            $tokenIssuedAt->apiTokenIssuedAt = date_create_from_format("Y-m-d H:i:s", $args["tokenIssuedAt"]);
+            $this->tokenIssuedAtHandler->handle($tokenIssuedAt);
         }
 
         return $this->userNormalizer->normalize($user->getUser());
