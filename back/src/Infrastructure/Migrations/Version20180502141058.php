ed6141f4d33cdf634d2fd5296d82bce4fc01263a
Nii Apa Abbey <abbeyniiapa@gmail.com>
2018-05-02 14:11:17 +0000

    Generated migration file for adding multiLogin field to User

---
+++ back/src/Infrastructure/Migrations/Version20180502141058.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Proximum\Vimeet\Infrastructure\Migrations;
+
+use Doctrine\DBAL\Migrations\AbstractMigration;
+use Doctrine\DBAL\Schema\Schema;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+class Version20180502141058 extends AbstractMigration
+{
+    /**
+     * @param Schema $schema
+     */
+    public function up(Schema $schema)
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE user ADD multi_login TINYINT(1) NOT NULL');
+    }
+
+    /**
+     * @param Schema $schema
+     */
+    public function down(Schema $schema)
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE user DROP multi_login');
+    }
+}
