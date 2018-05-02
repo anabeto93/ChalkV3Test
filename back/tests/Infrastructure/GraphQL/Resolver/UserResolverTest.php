f116c08c239cef3b39790fd3874405501a005b45
Nii Apa Abbey <abbeyniiapa@gmail.com>
2018-05-02 01:43:08 +0000

    Fixed and added test to UserResolverTest

--- back/tests/Infrastructure/GraphQL/Resolver/UserResolverTest.php
+++ back/tests/Infrastructure/GraphQL/Resolver/UserResolverTest.php
@@ -10,14 +10,16 @@
 
 namespace Tests\Infrastructure\GraphQL\Resolver;
 
+use App\Application\Command\User\TokenIssuedAt;
+use App\Application\Command\User\TokenIssuedAtHandler;
 use App\Domain\Model\User;
 use App\Infrastructure\GraphQL\Resolver\UserResolver;
 use App\Infrastructure\Normalizer\UserNormalizer;
 use App\Infrastructure\Security\Api\ApiUserAdapter;
 use GraphQL\Error\UserError;
+use Overblog\GraphQLBundle\Definition\Argument;
 use PHPUnit\Framework\TestCase;
 use Prophecy\Prophecy\ObjectProphecy;
-use Sonata\IntlBundle\Templating\Helper\LocaleHelper;
 use Symfony\Component\Security\Core\Authentication\Token\PreAuthenticatedToken;
 use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorage;
 
@@ -30,12 +32,16 @@ class UserResolverTest extends TestCase
     private $userNormalizer;
 
     /** @var ObjectProphecy */
+    private $tokenIssuedAtHandler;
+
+    /** @var ObjectProphecy */
     private $token;
 
     public function setUp()
     {
         $this->tokenStorage = $this->prophesize(TokenStorage::class);
         $this->userNormalizer = $this->prophesize(UserNormalizer::class);
+        $this->tokenIssuedAtHandler = $this->prophesize(TokenIssuedAtHandler::class);
         $this->token = $this->prophesize(PreAuthenticatedToken::class);
     }
 
@@ -48,9 +54,10 @@ class UserResolverTest extends TestCase
 
         $userResolver = new UserResolver(
             $this->tokenStorage->reveal(),
-            $this->userNormalizer->reveal()
+            $this->userNormalizer->reveal(),
+            $this->tokenIssuedAtHandler->reveal()
         );
-        $userResolver->resolveUser();
+        $userResolver->resolveUser(new Argument([]));
     }
 
     public function testResolveUser()
@@ -72,9 +79,56 @@ class UserResolverTest extends TestCase
 
         $userResolver = new UserResolver(
             $this->tokenStorage->reveal(),
-            $this->userNormalizer->reveal()
+            $this->userNormalizer->reveal(),
+            $this->tokenIssuedAtHandler->reveal()
         );
-        $result = $userResolver->resolveUser();
+        $result = $userResolver->resolveUser(new Argument([]));
+
+        $expected = [
+            'uuid' => 'uuid-user',
+            'firstName' => 'Jean',
+            'lastName' => 'Paul',
+            'phoneNumber' => '+33123123123',
+            'countryCode' => 'FR',
+            'country' => 'France',
+            'locale' => 'fr',
+        ];
+
+        $this->assertEquals($expected, $result);
+    }
+
+    public function testResolveUserTokenIssuedAt()
+    {
+        $user = $this->prophesize(User::class);
+        $apiUser = $this->prophesize(ApiUserAdapter::class);
+
+        $apiUser->getUser()->willReturn($user->reveal());
+
+        $this->tokenStorage->getToken()->shouldBeCalled()->willReturn($this->token->reveal());
+        $this->token->getUser()->shouldBeCalled()->willReturn($apiUser->reveal());
+
+        $tokenIssuedAt = new TokenIssuedAt($user->reveal());
+        $tokenIssuedAt->apiTokenIssuedAt = date_create_from_format("U", "1525223975");
+
+        $this->tokenIssuedAtHandler->handle($tokenIssuedAt)->shouldBeCalled();
+        $user->getApiTokenIssuedAt()->shouldBeCalled()->willReturn(null);
+
+        $this->userNormalizer->normalize($user)->shouldBeCalled()->willReturn([
+            'uuid' => 'uuid-user',
+            'firstName' => 'Jean',
+            'lastName' => 'Paul',
+            'phoneNumber' => '+33123123123',
+            'countryCode' => 'FR',
+            'country' => 'France',
+            'locale' => 'fr',
+        ]);
+
+        $userResolver = new UserResolver(
+            $this->tokenStorage->reveal(),
+            $this->userNormalizer->reveal(),
+            $this->tokenIssuedAtHandler->reveal()
+        );
+        $result = $userResolver->resolveUser(new Argument(["tokenIssuedAt" => "1525223975"]));
 
         $expected = [
             'uuid' => 'uuid-user',
