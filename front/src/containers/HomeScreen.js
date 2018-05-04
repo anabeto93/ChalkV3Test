4a1434263d490a2d4117709a78c96525edf870d7
Nii Apa Abbey <abbeyniiapa@gmail.com>
2018-05-04 00:54:20 +0000

    Changed HomeScreen to login input form

--- front/src/containers/HomeScreen.js
+++ front/src/containers/HomeScreen.js
@@ -1,19 +1,75 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { Redirect } from 'react-router-dom';
-import { COURSES } from '../config/routes';
+import { COURSES, LOGIN } from '../config/routes';
 import { LOGIN_STATE_LOGGED_IN } from '../store/defaultState';
 import I18n from 'i18n-js';
+import { Card, CardActions, CardHeader, CardText } from 'material-ui/Card';
+import TextField from 'material-ui/TextField';
+import RaisedButton from 'material-ui/RaisedButton';
+import generateUrl from '../services/generateUrl';
 
 export class HomeScreen extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      token: ''
+    };
+  }
+
+  handleChange = e => {
+    this.setState({
+      token: e.target.value
+    });
+  };
+
+  handleLogin = () => {
+    const { history } = this.props;
+    return history.push(
+      generateUrl(LOGIN, {
+        ':token': this.state.token
+      })
+    );
+  };
+
   render() {
     if (this.props.loggedIn) {
       return <Redirect to={COURSES} />;
     }
 
     return (
-      <div className="screen-centered alert">
-        {I18n.t('tokenError.401')}
+      <div className="screen-centered">
+        <Card>
+          <CardHeader
+            title={I18n.t('login.title')}
+            subtitle={I18n.t('login.message')}
+            actAsExpander={true}
+            showExpandableButton={true}
+            style={{ textAlign: 'left' }}
+          />
+
+          <CardText>
+            <TextField
+              fullWidth={true}
+              hintText={I18n.t('login.tokenPlaceholder')}
+              onChange={this.handleChange}
+              value={this.state.token}
+            />
+          </CardText>
+
+          <CardActions>
+            <RaisedButton
+              primary={true}
+              label={I18n.t('login.title')}
+              onClick={this.handleLogin}
+              disabled={!(this.state.token.length === 6)}
+            />
+          </CardActions>
+
+          <CardText expandable={true}>
+            {I18n.t('login.hint')}
+          </CardText>
+        </Card>
       </div>
     );
   }
