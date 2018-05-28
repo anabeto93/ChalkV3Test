e1dbab2b17cbf077f2fc8150636d05a91dda7485
Nii Apa Abbey <abbeyniiapa@gmail.com>
2018-05-28 17:33:56 +0000

    Changed session and header navigation, and route title resolving for session

--- src/components/SessionFooter.js
+++ src/components/SessionFooter.js
@@ -4,60 +4,19 @@ import Arrow from '@material-ui/icons/KeyboardArrowRight';
 import React from 'react';
 import { connect } from 'react-redux';
 import { withRouter } from 'react-router-dom';
-import {
-  SESSION_DETAIL,
-  SESSION_LIST,
-  SESSION_SEND,
-  QUESTION_DETAIL
-} from '../config/routes';
-import CourseManager from '../services/CourseManager';
-import generateUrl from '../services/generateUrl';
+import sessionNext from '../services/session/sessionNext';
 
 const footer = props => {
-  const { courseUuid, session, sessions, history, locale } = props;
-
-  const handleNext = () => {
-    if (session.questions && !session.validated) {
-      return history.push(
-        generateUrl(QUESTION_DETAIL, {
-          ':courseUuid': courseUuid,
-          ':sessionUuid': session.uuid,
-          ':questionIndex': 0
-        })
-      );
-    }
-
-    if (session.needValidation && !session.validated) {
-      return history.push(
-        generateUrl(SESSION_SEND, {
-          ':courseUuid': courseUuid,
-          ':sessionUuid': session.uuid
-        })
-      );
-    }
-
-    const nextSession = CourseManager.getNextSession(sessions, session);
-
-    if (nextSession !== null) {
-      return history.push(
-        generateUrl(SESSION_DETAIL, {
-          ':courseUuid': nextSession.courseUuid,
-          ':sessionUuid': nextSession.uuid
-        })
-      );
-    }
-
-    return history.push(
-      generateUrl(SESSION_LIST, {
-        ':courseUuid': session.courseUuid,
-        ':folderUuid': session.folderUuid
-      })
-    );
-  };
+  const { session, locale } = props;
 
   return (
     <footer className="next-session-footer background-grey">
-      <Button variant="raised" color="primary" onClick={handleNext} fullWidth>
+      <Button
+        variant="raised"
+        color="primary"
+        onClick={() => sessionNext(props)}
+        fullWidth
+      >
         {I18n.t(
           session.questions && !session.validated
             ? 'session.quizButton'
