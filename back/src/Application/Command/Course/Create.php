bcbf603dffbcc1e6526eaa71d8dc5682fc65dbe1
Nii Apa Abbey <abbeyniiapa@gmail.com>
2018-06-02 19:37:39 +0000

    Added institution requirement to Course Create command

--- back/src/Application/Command/Course/Create.php
+++ back/src/Application/Command/Course/Create.php
@@ -10,6 +10,18 @@
 
 namespace App\Application\Command\Course;
 
+use App\Domain\Model\Institution;
+
 class Create extends AbstractCourseCommand
 {
+    /** @var Institution */
+    public $institution;
+
+    /**
+     * @param Institution $institution
+     */
+    public function __construct(Institution $institution)
+    {
+        $this->institution = $institution;
+    }
 }
