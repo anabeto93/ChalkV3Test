ee1ee8719a5c51beba54b83d5a0d195632be46e7
Nii Apa Abbey <abbeyniiapa@gmail.com>
2018-06-21 01:52:14 +0000

    Added numberOfStudents to InstitutionView

--- back/src/Application/View/Institution/InstitutionView.php
+++ back/src/Application/View/Institution/InstitutionView.php
@@ -22,17 +22,22 @@ class InstitutionView {
     /** @var int */
     public $numberOfCourses;
 
+    /** @var int */
+    public $numberOfStudents;
+
     /**
      * InstitutionView constructor.
      * @param int $id
      * @param string $name
      * @param int $numberOfCohorts
      * @param int $numberOfCourses
+     * @param int $numberOfStudents
      */
-    public function __construct(int $id, string $name, int $numberOfCohorts, int $numberOfCourses) {
+    public function __construct(int $id, string $name, int $numberOfCohorts, int $numberOfCourses, int $numberOfStudents) {
         $this->id = $id;
         $this->name = $name;
         $this->numberOfCohorts = $numberOfCohorts;
         $this->numberOfCourses = $numberOfCourses;
+        $this->numberOfStudents = $numberOfStudents;
     }
 }
