8c9800cb5c9200fc133e571c44c63c9b464f0a70
Nii Apa Abbey <abbeyniiapa@gmail.com>
2018-06-22 00:43:45 +0000

    Generated migration file

---
+++ back/src/Infrastructure/Migrations/Version20180622004315.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Proximum\Vimeet\Infrastructure\Migrations;
+
+use Doctrine\DBAL\Migrations\AbstractMigration;
+use Doctrine\DBAL\Schema\Schema;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+class Version20180622004315 extends AbstractMigration
+{
+    /**
+     * @param Schema $schema
+     */
+    public function up(Schema $schema)
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE user CHANGE institution_id institution_id INT NOT NULL');
+    }
+
+    /**
+     * @param Schema $schema
+     */
+    public function down(Schema $schema)
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE user CHANGE institution_id institution_id INT DEFAULT NULL');
+    }
+}
