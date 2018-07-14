aa1f22d6f153fe17213d70a5cb6b40cd7ff042d7
Nii Apa Abbey <abbeyniiapa@gmail.com>
2018-07-14 23:13:11 +0000

    Delete 'back' and 'ansible' folders

--- back/features/admin/home.feature
+++
@@ -1,4 +0,0 @@
-Feature: Home admin
-  Scenario: I can go to the home of the admin
-    When I go to "/admin/"
-    Then I should see "Welcome"
--- back/src/Infrastructure/InfrastructureBundle/InfrastructureBundle.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace App\Infrastructure\InfrastructureBundle;
-
-use Symfony\Component\HttpKernel\Bundle\Bundle;
-
-class InfrastructureBundle extends Bundle
-{
-}
