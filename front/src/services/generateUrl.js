05b5bf1db3e36353f6702c0cebeb028215b10841
Nii Apa Abbey <abbeyniiapa@gmail.com>
2018-07-16 12:24:29 +0000

    Switched to Laravel

--- front/src/services/generateUrl.js
+++
@@ -1,9 +0,0 @@
-export default function generateUrl(path, params = {}) {
-  let url = path;
-
-  for (let key in params) {
-    url = url.replace(key, params[key]);
-  }
-
-  return url;
-}
--- front/src/services/quiz/stringifyUserAnswers.js
+++
@@ -1,9 +0,0 @@
-export default function stringifyUserAnswers(questions) {
-  const answers = questions
-    .map((key, index) => {
-      return questions[index].userAnswers.join();
-    })
-    .join(';');
-
-  return answers;
-}
